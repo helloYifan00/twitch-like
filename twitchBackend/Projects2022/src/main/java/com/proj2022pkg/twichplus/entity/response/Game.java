69e010bc3ccff88985b453b60b19f4c8d6de9b04
helloYifan00 <104401919+helloYifan00@users.noreply.github.com>
2022-04-26 15:04:06 -0700

    Update Game.java

--- twitchBackend/Projects2022/src/main/java/com/proj2022pkg/twichplus/entity/response/Game.java
+++ twitchBackend/Projects2022/src/main/java/com/proj2022pkg/twichplus/entity/response/Game.java
@@ -5,18 +5,16 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
-// 反序列化 序列化??? 再聽一次 把不需要的數據discard
 @JsonIgnoreProperties(ignoreUnknown = true)
-@JsonInclude(JsonInclude.Include.NON_NULL) //值是"非空"才返回
-@JsonDeserialize(builder = Game.Builder.class) // 用這個class來創建???
-public class Game { //作為response返回給browser
+@JsonInclude(JsonInclude.Include.NON_NULL) 
+@JsonDeserialize(builder = Game.Builder.class) 
+public class Game { 
     @JsonProperty("id")
     private final String id;
 
     @JsonProperty("name")
     private final String name;
 
-    // @JsonProperty將這裡的參數和twitch的field做了mapping
     @JsonProperty("box_art_url")
     private final String boxArtUrl;
 
@@ -38,9 +36,6 @@ public class Game { //作為response返回給browser
         this.boxArtUrl = builder.boxArtUrl;
     }
 
-    // 透過Builder把對象轉化為JSON : String -> JSON
-    // 類的內部有很多成員變量，有些默認有些要另設值
-    // 所以Builder可以做統一處理 PS. search builder pattern
     @JsonIgnoreProperties(ignoreUnknown = true)
     @JsonInclude(JsonInclude.Include.NON_NULL)
     public static class Builder {
