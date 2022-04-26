000b39a9b3b78664864a41eb8a3611ba9c781638
helloYifan00 <104401919+helloYifan00@users.noreply.github.com>
2022-04-26 15:04:23 -0700

    Update LoginResponseBody.java

--- twitchBackend/Projects2022/src/main/java/com/proj2022pkg/twichplus/entity/response/LoginResponseBody.java
+++ twitchBackend/Projects2022/src/main/java/com/proj2022pkg/twichplus/entity/response/LoginResponseBody.java
@@ -2,15 +2,14 @@ package com.proj2022pkg.twichplus.entity.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class LoginResponseBody { // 返回LoginResponseBody的object
-    // 定義要返回什麼
+public class LoginResponseBody {
+ 
     @JsonProperty("user_id")
     private final String userId;
 
     @JsonProperty("name")
     private final String name;
 
-    //ResponseBody將目前Item object的field轉成JSON string傳回給前端
     public LoginResponseBody(String userId, String name) {
         this.userId = userId;
         this.name = name;
