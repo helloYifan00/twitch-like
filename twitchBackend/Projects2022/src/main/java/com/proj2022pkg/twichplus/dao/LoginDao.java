aa6f98256946175bda7ae8d53be712a9e9b11bf0
helloYifan00 <104401919+helloYifan00@users.noreply.github.com>
2022-04-26 15:01:37 -0700

    Update LoginDao.java

--- twitchBackend/Projects2022/src/main/java/com/proj2022pkg/twichplus/dao/LoginDao.java
+++ twitchBackend/Projects2022/src/main/java/com/proj2022pkg/twichplus/dao/LoginDao.java
@@ -10,15 +10,12 @@ import org.springframework.stereotype.Repository;
 public class LoginDao {
 
     @Autowired
-    private SessionFactory sessionFactory; // 連接到數據庫
-
-    // Verify if the given user Id and password are correct. Returns the user name when it passes
+    private SessionFactory sessionFactory; 
     public String verifyLogin(String userId, String password) {
         String name = "";
 
         try (Session session = sessionFactory.openSession()) {
-            // get操作其實是需要transaction(因為你在讀的時候可能別人在寫)，不過這裡的情況較簡單只會是單線程
-            User user = session.get(User.class, userId); // User.class就是要找User這個表
+            User user = session.get(User.class, userId); 
             if(user != null && user.getPassword().equals((password))) {
                 name = user.getFirstName();
             }
